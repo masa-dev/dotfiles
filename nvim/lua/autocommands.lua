60121855c2e5e5cadce5e216a74b3ac93f8ae74a
Masa <mssa.massan@gmail.com>
2024-10-07 00:43:12 +0900

    refactor(nvim): 自動コマンドの定義をinit.luaからautocommands.luaに移動
    - コードの可読性と整理のため、基本的な自動コマンド設定を分離
    - 将来的なメンテナンスを容易にするための構造改善

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -1,3 +1,17 @@
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 -- ファイルごとのインデント幅を調整
 local filetype_tabstop = { lua = 2, markdown = 2, javascript = 2, csharp = 4 }
 local usrftcfg = vim.api.nvim_create_augroup("UserFileTypeConfig", { clear = true })
