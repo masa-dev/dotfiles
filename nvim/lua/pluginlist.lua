c5102e57616340fe25e2fca16c48d35eaf5428ee
Masa <mssa.massan@gmail.com>
2024-11-04 00:22:44 +0900

    refactor(nvim): toggletermの設定をファイルとして分ける

--- nvim/lua/pluginlist.lua
+++ nvim/lua/pluginlist.lua
@@ -378,30 +378,7 @@ require("lazy").setup({
   {
     "akinsho/toggleterm.nvim",
     config = function()
-      local Terminal = require("toggleterm.terminal").Terminal
-      local lazygit = Terminal:new({
-        cmd = "lazygit",
-        dir = "git_dir",
-        direction = "float",
-        float_opts = {
-          border = "double",
-        },
-        -- function to run on opening the terminal
-        on_open = function(term)
-          vim.cmd("startinsert!")
-          vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
-        end,
-        -- function to run on closing the terminal
-        on_close = function()
-          vim.cmd("startinsert!")
-        end,
-      })
-
-      function LazygitToggle()
-        lazygit:toggle()
-      end
-
-      vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua LazygitToggle()<CR>", { noremap = true, silent = true })
+      require("pluginconfig.toggleterm")
     end,
   },
 
