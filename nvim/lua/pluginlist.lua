041463663f86e31e8ff6494d52aae3a19fb261c1
Masa <mssa.massan@gmail.com>
2024-11-04 02:47:29 +0900

    feat(nvim): indent-blankline.nvim を追加

--- nvim/lua/pluginlist.lua
+++ nvim/lua/pluginlist.lua
@@ -82,6 +82,14 @@ require("lazy").setup({
   },
 
   {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
+  },
+
+  {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
