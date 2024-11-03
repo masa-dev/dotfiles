6cebc8454c82cee04cdbe39987d5fb0579041fe7
Masa <mssa.massan@gmail.com>
2024-11-03 14:20:37 +0900

    feat(nvim): barbar.nvimプラグインの追加
    - vim.g.have_nerd_fontをtrueに設定し、Nerd Fontのサポートを有効化
    - barbar.nvimをプラグインリストに追加し、依存関係を設定
    - lazy-lock.jsonでbarbar.nvimとnvim-web-deviconsのコミット情報を追記

--- nvim/lua/pluginlist.lua
+++ nvim/lua/pluginlist.lua
@@ -64,6 +64,23 @@ require("lazy").setup({
     end,
   },
 
+  {
+    "romgrk/barbar.nvim",
+    dependencies = {
+      "lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
+      "nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+    },
+    init = function()
+      vim.g.barbar_auto_setup = false
+    end,
+    opts = {
+      sidebar_filetypes = {
+        ["neo-tree"] = true,
+      },
+    },
+    version = "^1.0.0", -- optional: only update when a new 1.x version is released
+  },
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
