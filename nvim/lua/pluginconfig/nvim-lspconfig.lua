f032c1cdf012f9d5481d450e37a91a6c97e4967d
Masa <mssa.massan@gmail.com>
2024-11-10 21:34:40 +0900

    feat(nvim): マッピングの追加、サジェスト確定をTabで行えるようにする

--- nvim/lua/pluginconfig/nvim-lspconfig.lua
+++ nvim/lua/pluginconfig/nvim-lspconfig.lua
@@ -73,6 +73,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
     -- or a suggestion from your LSP for this to activate.
     map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
 
+    map("<leader>ch", vim.lsp.buf.hover, "[C]ode [H]over", { "n", "x" })
+
     -- WARN: This is not Goto Definition, this is Goto Declaration.
     --  For example, in C this would take you to the header.
     map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
