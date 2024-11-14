83cf1a1d696fdf8c4bbccf00712657c385a9c361
Masa <mssa.massan@gmail.com>
2024-11-14 23:29:21 +0900

    feat(nvim): ToggleTermの設定

--- nvim/lua/pluginconfig/toggleterm.lua
+++ nvim/lua/pluginconfig/toggleterm.lua
@@ -1,3 +1,5 @@
+require("toggleterm").setup()
+
 local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({
   cmd = "lazygit",
@@ -26,4 +28,32 @@ vim.api.nvim_set_keymap(
   "<leader>tg",
   "<cmd>lua LazygitToggle()<CR>",
   { desc = "[T]oggle [G]it Terminal (lazygit)", noremap = true, silent = true }
+)
+
+-- ターミナル
+local floatTerm = Terminal:new({
+  direction = "float",
+  float_opts = {
+    border = "double",
+  },
+  -- function to run on opening the terminal
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+  end,
+  -- function to run on closing the terminal
+  on_close = function()
+    vim.cmd("startinsert!")
+  end,
+})
+
+function FloatTermToggle()
+  floatTerm:toggle()
+end
+
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>tt",
+  "<cmd>lua FloatTermToggle()<CR>",
+  { desc = "[T]oggle [T]erminal", noremap = true, silent = true }
 )
