287b632e39f1f732d48ea6087c3f284f9782d596
masa-dev <mssa.massan@gmail.com>
2025-07-18 13:21:07 +0900

    feat(lsp): vueのLSP設定の追加

--- nvim/lua/pluginconfig/nvim-lspconfig.lua
+++ nvim/lua/pluginconfig/nvim-lspconfig.lua
@@ -185,17 +185,6 @@ local servers = {
       "typescript",
       "typescriptreact",
       "typescript.tsx",
-      "vue",
-    },
-    init_options = {
-      hostInfo = "neovim",
-      plugins = {
-        {
-          name = "@vue/typescript-plugin",
-          location = "~/.nvm/versions/node/v22.11.0/lib/node_modules/@vue/typescript-plugin",
-          languages = { "javascript", "typescript", "vue" },
-        },
-      },
     },
   },
 }
@@ -229,3 +218,60 @@ require("mason-lspconfig").setup({
     end,
   },
 })
+
+-- Volarの設定
+local vue_language_server_path = vim.fn.expand("$MASON/packages")
+  .. "/vue-language-server"
+  .. "/node_modules/@vue/language-server"
+
+local vue_plugin = {
+  name = "@vue/typescript-plugin",
+  location = vue_language_server_path,
+  languages = { "vue" },
+  configNamespace = "typescript",
+}
+local vtsls_config = {
+  settings = {
+    vtsls = {
+      tsserver = {
+        globalPlugins = {
+          vue_plugin,
+        },
+      },
+    },
+  },
+  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+}
+
+local vue_ls_config = {
+  on_init = function(client)
+    client.handlers["tsserver/request"] = function(_, result, context)
+      local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "vtsls" })
+      if #clients == 0 then
+        vim.notify("Could not find `vtsls` lsp client, `vue_ls` would not work without it.", vim.log.levels.ERROR)
+        return
+      end
+      local ts_client = clients[1]
+
+      local param = unpack(result)
+      local id, command, payload = unpack(param)
+      ---@diagnostic disable-next-line: undefined-field
+      ts_client:exec_cmd({
+        title = "vue_request_forward", -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
+        command = "typescript.tsserverRequest",
+        arguments = {
+          command,
+          payload,
+        },
+      }, { bufnr = context.bufnr }, function(_, r)
+        local response_data = { { id, r.body } }
+        ---@diagnostic disable-next-line: param-type-mismatch
+        client:notify("tsserver/response", response_data)
+      end)
+    end
+  end,
+}
+
+local lspconfig = require("lspconfig")
+lspconfig.vtsls.setup(vtsls_config)
+lspconfig.vue_ls.setup(vue_ls_config)
